66355b36f3efc1f31443bd052c8f95f7768b5935
jaywant.waychal@gmail.com <jaywant.waychal@gmail.com>
2021-01-09 12:21:42 -0800

    assignment-1

---
+++ src/main/java/com/jd/spc/SpcAssignment1Application.java
@@ -0,0 +1,13 @@
+package com.jd.spc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpcAssignment1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpcAssignment1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/jd/spc/SpcAssignment1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jd.spc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpcAssignment1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
